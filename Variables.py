342411dd8c6bd7d1272cd47210e6902bf4907f08
SupriyoMallick <37034993+SupriyoMallick@users.noreply.github.com>
2019-11-17 16:51:27 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,18 @@
+x = 5
+y ='Hello'
+print(x)
+print(y)
+_name='Supriya'
+_title='Mallick'
+print(_name)
+print(_title)
+
+name, age, dept='Supriya',30,'IT'
+#this is a comments 
+#print('name '+name)
+print('dept '+dept)
+print (age)
+
+"""this is
+my comments"""
+
